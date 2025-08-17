13eba84ef5986dcde5ee4783dc0bb426eaf888bb
William Pettersson <contact@williampettersson.eu>
2025-08-17 17:14:02 +0200

    Add function to display stored courses as a table

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const courseNameField: HTMLInputElement = document.getElementById("course-name")
 const courseProgField: HTMLInputElement = document.getElementById("course-prog") as HTMLInputElement;
 const courseSyllField: HTMLInputElement = document.getElementById("course-syll") as HTMLInputElement;
 const infoBox: HTMLDivElement = document.getElementById("info-box") as HTMLDivElement;
+const courseTable: HTMLTableElement = document.getElementById("course-table") as HTMLTableElement;
 
 // Create interface for courses
 interface CourseInfo {
@@ -37,6 +38,8 @@ function addCourse(): void {
 		courseNameField.value = "";
 		courseProgField.value = "";
 		courseSyllField.value = "";
+		// Update table
+		displayCourses();
 	}
 }
 
@@ -68,5 +71,51 @@ function isCourseValid(newCourse: CourseInfo): Boolean {
 	}
 }
 
+// Populate course table
+function displayCourses(): void {
+	// Clear table
+	courseTable.innerHTML = "";
+	// Add header
+	let headerRow: HTMLTableRowElement = document.createElement("tr");
+	let headerCode: HTMLTableCaptionElement = document.createElement("th");
+	headerCode.textContent = "Kurskod";
+	let headerName: HTMLTableCaptionElement = document.createElement("th");
+	headerName.textContent = "Kursnamn";
+	let headerProg: HTMLTableCaptionElement = document.createElement("th");
+	headerProg.textContent = "Progression";
+	headerRow.appendChild(headerCode);
+	headerRow.appendChild(headerName);
+	headerRow.appendChild(headerProg);
+	courseTable.appendChild(headerRow);
+	// Add cells
+	const storedCourses: string | null = localStorage.getItem("courses");
+	if (storedCourses) { // Check if any courses are stored
+		const parsedCourses: Array<CourseInfo> = JSON.parse(storedCourses);
+		parsedCourses.forEach(storedCourse => {
+			// Add a new row
+			let newRow: HTMLTableRowElement = document.createElement("tr");
+			// Add a course code cell with link to syllabus
+			let newCode: HTMLTableCellElement = document.createElement("td");
+			let newSyll: HTMLAnchorElement = document.createElement("a");
+			newSyll.href = storedCourse.syllabus;
+			newSyll.textContent = storedCourse.code;
+			newCode.appendChild(newSyll);
+			// Add a course name cell
+			let newName: HTMLTableCellElement = document.createElement("td");
+			newName.textContent = storedCourse.name;
+			// Add a course progression cell
+			let newProg: HTMLTableCellElement = document.createElement("td");
+			newProg.textContent = storedCourse.progression;
+			// Add cells to row
+			newRow.appendChild(newCode);
+			newRow.appendChild(newName);
+			newRow.appendChild(newProg);
+			// Add row to table
+			courseTable.appendChild(newRow);
+		});
+	}
+}
+
 // Event handlers
 addButton.addEventListener("click", addCourse);
+addEventListener("load", displayCourses);
